ae1ef88b014dd4c35b528a1dba5ac82ec0192419
Hieu Dang <dangtrunghieu0207@gmail.com>
2021-02-26 20:21:57 +0700

    Implement quick edit.

---
+++ resources/js/manage-user/user-list/user_list.ts
@@ -0,0 +1,57 @@
+interface UserListParams {
+    id: number | string;
+    name: string;
+    date_created: string;
+    role: string;
+    status: string;
+}
+
+export default class UserList {
+    protected listUser;
+
+    constructor(list_user: UserListParams[]) {
+        this.listUser = list_user;
+    }
+
+    renderUserList(): void {
+        let templateUser = '';
+        this.listUser.forEach(user => {
+            templateUser += `<tr class="js-user-${user.id}">
+                    <td><span class="user-id">${user.id}</span></td>
+                    <td><a href="#"><img src="/examples/images/avatar/2.jpg" class="avatar" alt="Avatar"> <span class="user-name">${user.name}</span></a></td>
+                    <td><span class="date_created">${user.date_created}</span></td>
+                    <td>${user.role}</td>
+                    <td><span class="status text-success">&bull;</span> ${user.status}</td>
+                    <td>
+                        <a href="#" class="settings"  data-value="${user.id}" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+                        <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                    </td>
+                </tr>`;
+        });
+        const el = document.querySelector('.user-list');
+        if (el) {
+            el.innerHTML = templateUser;
+            const settings = document.querySelectorAll('.settings');
+            settings.forEach(setting => {
+                setting.addEventListener('click', () => {
+                    const data = setting.getAttribute('data-value');
+                    if (data) {
+                        this.editHandler(data);
+                    }
+                });
+            });
+        }
+    }
+
+    editHandler(id: string): void {
+        const userClass = '.js-user-' + id;
+        const userEl = document.querySelector(userClass);
+        const userNameElement = userEl!.querySelector('.user-name');
+        const newInputElement = document.createTextNode('input');
+        userNameElement!.replaceChild(newInputElement, userNameElement);
+    }
+
+    render(): void {
+        this.renderUserList();
+    }
+}
---
+++ resources/views/manage-user/user-list.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Bootstrap User Management Data Table</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href={{asset('css/common/bootstrap.min.css')}}>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href={{asset('css/common/font-awesome.min.css')}}>
+    <link rel="stylesheet" href={{asset('css/manage-user/user-list/style.css')}}>
+    <script src={{asset('js/common/jquery-3.5.1.min.js')}}></script>
+    <script src={{asset('js/common/popper.min.js')}}></script>
+    <script src={{asset('js/common/bootstrap.min.js')}}></script>
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+<body>
+<div class="container-xl">
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <h2>User <b>Management</b></h2>
+                    </div>
+                    <div class="col-sm-7">
+                        <a href="#" class="btn btn-secondary"><i class="material-icons">&#xE147;</i> <span>Add New User</span></a>
+                        <a href="#" class="btn btn-secondary"><i class="material-icons">&#xE24D;</i> <span>Export to Excel</span></a>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Date Created</th>
+                    <th>Role</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody class="user-list">
+
+                </tbody>
+            </table>
+            <div class="clearfix">
+                <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+                <ul class="pagination">
+                    <li class="page-item disabled"><a href="#">Previous</a></li>
+                    <li class="page-item"><a href="#" class="page-link">1</a></li>
+                    <li class="page-item"><a href="#" class="page-link">2</a></li>
+                    <li class="page-item active"><a href="#" class="page-link">3</a></li>
+                    <li class="page-item"><a href="#" class="page-link">4</a></li>
+                    <li class="page-item"><a href="#" class="page-link">5</a></li>
+                    <li class="page-item"><a href="#" class="page-link">Next</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<script src={{asset('js/manage-user/index.js')}}></script>
+</body>
+</html>
